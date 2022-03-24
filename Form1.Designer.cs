948c4dd3a2ad572459ea7701579e2e5cef40b3fa
din0025-mattyv <102110216+din0025-mattyv@users.noreply.github.com>
2022-03-24 12:19:58 +1100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace SofDev_SAC_One_Task_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.grdvwData = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdvwData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(17, 28);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(144, 51);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Calculate Profit";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // grdvwData
+            // 
+            this.grdvwData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdvwData.Location = new System.Drawing.Point(12, 99);
+            this.grdvwData.Name = "grdvwData";
+            this.grdvwData.ReadOnly = true;
+            this.grdvwData.RowHeadersWidth = 62;
+            this.grdvwData.RowTemplate.Height = 28;
+            this.grdvwData.Size = new System.Drawing.Size(1171, 516);
+            this.grdvwData.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1195, 627);
+            this.Controls.Add(this.grdvwData);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grdvwData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.DataGridView grdvwData;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SofDev_SAC_One_Task_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //initialise the number of columns
+            grdvwData.ColumnCount = 7;
+            //add columns to datagridview
+            grdvwData.Columns[0].Name = @"Name";
+            grdvwData.Columns[1].Name = "Category";
+            grdvwData.Columns[2].Name = "Seller";
+            grdvwData.Columns[3].Name = "Purchaser";
+            grdvwData.Columns[4].Name = "Purchase price";
+            grdvwData.Columns[5].Name = "Sale price";
+            grdvwData.Columns[6].Name = "Profit";
+        }
+
+        public void loadDataGrid()
+        {
+            float totProfit = 0f;
+
+            //initialise lines variable
+            var lines = new List<string>();
+
+            //create list of the file from a file dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                string filePath = openFileDialog.FileName;
+                lines = File.ReadAllLines(filePath).ToList();
+            }
+
+            foreach(string line in lines)
+            {
+                //create list of lines split by comma
+                List<string> splitData = line.Split(',').ToList();
+
+                float profit = 0f;
+
+                //parse the purchase price value
+                float.TryParse(splitData[4], out float purchasePrice);
+                //create a bool of the sale price value's parse success
+                //to make an if statement simple and easy to read
+                bool saleValRet = float.TryParse(splitData[5], out float salePrice);
+
+
+                //calculate the profit using these variables,
+                //if sale value is N/A, set the profit to a negative
+
+                if (saleValRet == true)
+                {
+
+                    profit = salePrice - purchasePrice;
+
+                }
+                else
+                {
+                    profit = purchasePrice * -1;
+
+                }
+
+
+                //add profit to the total
+                totProfit += profit;
+                //add profit to the list in a currency form
+                splitData.Add(profit.ToString("c"));
+                //add list to datagridview
+                grdvwData.Rows.Add(splitData.ToArray());
+
+
+            }
+            //ensure total profit is displayed correctly
+            grdvwData.Rows.Add();
+            int rowCount = grdvwData.Rows.Count;
+            grdvwData.Rows[rowCount - 1].Cells[5].Value = "Total profit";
+            //display the total profit in a currency format
+            grdvwData.Rows[rowCount - 1].Cells[6].Value = totProfit.ToString("c");
+
+        }
+
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            loadDataGrid();
+
+        }
+    }
+}
